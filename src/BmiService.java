cb9c693fcf68363320618f4c6fb0b7c00834a670
Ekaterina Saukova <esaukova@outlook.com>
2023-06-26 20:44:48 +0300

    Расчёт индекса массы тела

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,9 +1,8 @@
 public class BmiService {
-    public double calculateBmi(double weight, double height) {
+    public int calculateBmi(double weight, double height) {
 
         double bmi = (weight / Math.pow(height, 2));
-        double roundBmi = Math.round(bmi);
-        return (int) roundBmi;
+        return (int) bmi;
 
     }
 }
